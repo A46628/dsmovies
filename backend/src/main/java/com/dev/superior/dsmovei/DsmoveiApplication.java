10eecdc68472c31bebc47ddab0699974bcb8e7ba
A46628 <A46628@alunos.isel.pt>
2022-03-08 09:17:54 +0000

    Meu Primeiro commit

---
+++ backend/src/main/java/com/dev/superior/dsmovei/DsmoveiApplication.java
@@ -0,0 +1,18 @@
+package com.dev.superior.dsmovei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DsmoveiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmoveiApplication.class, args);
+		System.out.print("tomara que funciona como e do esperado");
+		///.out("vendo se realmente funciona como e do esperado");		//71f2d667-8122-40d2-9103-443d4efd43c7
+		;
+	}
+
+
+}
---
+++ backend/src/test/java/com/dev/superior/dsmovei/DsmoveiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.superior.dsmovei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmoveiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
